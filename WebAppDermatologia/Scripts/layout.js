6716466753fcd68faa3ab701d3839a4a57bc3859
Ayrton Avalos <ayrtonfidel9@gmail.com>
2021-02-14 19:27:02 -0500

    Se agrego el model con entity framework Se termino la plantilla (layout) para la interfaz y un poco del inicio

--- WebAppDermatologia/Scripts/layout.js
+++ WebAppDermatologia/Scripts/layout.js
@@ -3,14 +3,43 @@
 act.addEventListener("change", () => {
     if (act.checked == true) {
         document.getElementById("menuIzq").style.width = '20%';
-        document.querySelector("#ops").removeAttribute("class", "hidden");
+        mostrarLetras(document.getElementsByClassName('letras'));
+        cambiarTamanio(document.getElementsByClassName("opcionMn"), '7.15em');
     }
     else {
         document.getElementById("menuIzq").style.width = '6%';
-        document.querySelector("#ops").setAttribute("class", "hidden");
+        esconderLetras();
+        cambiarTamanio(document.getElementsByClassName("opcionMn"), '2.364em');
     }
 });
 
-var act = document.getElementById("act");
+function cambiarTamanio(elem, tam)
+{
+    for (let i = 0; i < elem.length; i++)
+    {
+        elem[i].style.width = tam;
+    }
+}
+var elementos = [];
+function mostrarLetras(elem)
+{
+    let max = elem.length;
+    
+    for (let i = 0; i < max; i++)
+    {
+        elementos.push(elem[i]);
+    }   
+
+    for (let i = 0; i < elementos.length; i++)
+    {
+        elementos[i].removeAttribute("class", "hidden");
+    }
+}
+
+function esconderLetras() {
+    for (let i = 0; i < elementos.length; i++) {
+        elementos[i].setAttribute("class", "hidden");
+    }
+}
 
 
